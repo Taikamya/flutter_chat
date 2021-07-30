f06da56135e7414a043dc54993f2bab0e5e6cb06
Dimitri Dias Moreira <taikamya@gmail.com>
2021-07-30 01:22:00 -0300

    additions

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-import time
+import time, os
 
 app = Flask(__name__)
 @app.route("/bot", methods=["POST"])
@@ -13,4 +13,7 @@ def response():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",)
+
+    port = int(os.environ.get('PORT', 5000))
+
+    app.run(host="0.0.0.0", port=port)
