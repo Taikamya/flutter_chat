57d5a0b0535eabed8e39f636792f91af7b2dc1c6
Dimitri Dias Moreira <taikamya@gmail.com>
2021-08-10 23:05:17 -0300

    small changes

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, jsonify, request
 import time, os
 
+
 app = Flask(__name__)
-@app.route("/bot", methods=["POST"])
 
 
-# response
+@app.route("/bot", methods=["POST"])  # response
 def response():
     query = dict(request.form)['query']
     result = query + " " + time.ctime()
@@ -13,7 +13,5 @@ def response():
 
 
 if __name__ == "__main__":
-
     port = int(os.environ.get('PORT', 5000))
-
     app.run(host="0.0.0.0", port=port)
